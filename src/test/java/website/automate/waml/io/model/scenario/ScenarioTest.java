b40a22b5c3fdf8ba5ac75e2a1799399a2ef9ecf3
Michael Lewkowski <michael@lewkowski.de>
2017-07-31 22:54:57 +0200

    added more scenario tests

--- src/test/java/website/automate/waml/io/model/scenario/ScenarioTest.java
+++ src/test/java/website/automate/waml/io/model/scenario/ScenarioTest.java
@@ -1,30 +1,54 @@
 package website.automate.waml.io.model.scenario;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
 public class ScenarioTest {
 
   private static final String SCENARIO_NAME = "foo";
-  
+
   @Test
-  public void scenariosAreEqualWhenNameEquals(){
+  public void scenariosAreEqualWhenNameEqual() {
     Scenario left = createScenario(SCENARIO_NAME);
     Scenario right = createScenario(SCENARIO_NAME);
-    
+
     assertEquals(left, right);
   }
-  
+
   @Test
-  public void scnearioHashCodesAreEqualWhenNameEquals(){
+  public void scenarioHashCodesAreEqualWhenNameEqual() {
     Scenario left = createScenario(SCENARIO_NAME);
     Scenario right = createScenario(SCENARIO_NAME);
-    
+
     assertEquals(left.hashCode(), right.hashCode());
   }
-  
-  private Scenario createScenario(String name){
+
+  @Test
+  public void scenarioIsNotEqualToNull() {
+    Scenario left = createScenario(SCENARIO_NAME);
+
+    assertNotEquals(left, null);
+  }
+
+  @Test
+  public void scenariosAreNotEqualWhenNameNotEqual() {
+    Scenario left = createScenario(SCENARIO_NAME);
+    Scenario right = createScenario("other");
+
+    assertNotEquals(left, right);
+  }
+
+  @Test
+  public void scenariosAreNotEqualWhenNameOfOneIsNull() {
+    Scenario left = createScenario(SCENARIO_NAME);
+    Scenario right = createScenario(null);
+
+    assertNotEquals(left, right);
+  }
+
+  private Scenario createScenario(String name) {
     return new Scenario(name, null, null, null, null, null, null);
   }
 }
