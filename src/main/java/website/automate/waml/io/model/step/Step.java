9c3dd9ea626663d7354e392708bcb10afcfa335e
Michael Lewkowski <michael@lewkowski.de>
2017-09-17 22:27:22 +0200

    add register decorator tests

--- src/main/java/website/automate/waml/io/model/step/Step.java
+++ src/main/java/website/automate/waml/io/model/step/Step.java
@@ -2,7 +2,7 @@ package website.automate.waml.io.model.step;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"when", "unless", "invert", "register", "timeout"})
+@JsonPropertyOrder({"when", "unless", "invert", "timeout", "register"})
 public interface Step {
 
   String getWhen();
